e1c8a801bc9a974eceab5c57302ba569af3c0d04
MaaaaaaaSa <yakyubou.1222@icloud.com>
2022-06-21 12:25:11 +0900

    新規登録、ログイン画面のビュー作成

--- db/migrate/20220620233554_devise_create_users.rb
+++ db/migrate/20220620233554_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :phone,              null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.integer :area, null: false
+      t.integer :area_id, null: false
 
       ## Recoverable
       t.string   :reset_password_token
