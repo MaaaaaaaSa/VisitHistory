4f42971ad3e82371e0858be332ffb45ffe72f348
MaaaaaaaSa <yakyubou.1222@icloud.com>
2022-06-23 14:38:18 +0900

    customersテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tops#index'
   resources :menus, only: [:index, :create]
-  resources :customers
+  resources :customers, only: [:index, :show]
   resources :tops
 end
